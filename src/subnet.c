c77e2036ec3441a33463dcc4297d84d0e430a4bc
Andreas Hrubak <bandie9100@gmail.com>
2018-02-16 12:36:29 +0100

    support file paths in cidr/alias definitions

--- src/subnet.c
+++ src/subnet.c
@@ -6,11 +6,22 @@
 #include <err.h>
 #include <errno.h>
 
-#define NETNAME_MAX_LEN 63
+#define NETNAME_MAX_LEN 127
+#define NETNAME_MAX_LEN_STR "127"
+#define CIDR_MAX_LEN 43
 #define CIDR_FILE "/etc/cidr"
 #define ADDRLEN_BY_AF(x)	( (((x) == AF_INET) ? sizeof(struct in_addr) : ((x) == AF_INET6) ? sizeof(struct in6_addr) : 0)*8 )
 #define YESSLURP 1
 #define NOSLURP 0
+#define EXIT_UNKNOWN_SUBNET (EXIT_FAILURE + 1)
+#define EXIT_PARSE_ERROR    (EXIT_FAILURE + 2)
+#define EXIT_SYS_ERROR      (EXIT_FAILURE + 3)
+#define IN_NAMED_SUBNET_YES   0
+#define IN_NAMED_SUBNET_NO    1
+#define IN_NAMED_SUBNET_WRONG 2
+#define TRUE 1
+#define FALSE 0
+
 
 
 char *cidr_file;
@@ -124,11 +135,11 @@ int snmask(const char *cidr, struct ipaddr *addr)
 	const char *ptr;
 
 	ptr = strchr(cidr, '/');
-	if(ptr == NULL) return 0;
+	if(ptr == NULL) return FALSE;
 	ptr++;
 	addr->mask = atoi(ptr);
-	if(addr->mask > ADDRLEN_BY_AF(addr->af)) return 0;
-	return 1;
+	if(addr->mask > ADDRLEN_BY_AF(addr->af)) return FALSE;
+	return TRUE;
 }
 
 int parseIpStr(const char *str, struct ipaddr *addr)
@@ -148,7 +159,7 @@ int parseIpStr(const char *str, struct ipaddr *addr)
 	if(ok == -1)
 	{
 		perror("inet_pton");
-		exit(3);
+		exit(EXIT_PARSE_ERROR);
 	}
 	return ok;
 }
@@ -156,12 +167,12 @@ int parseIpStr(const char *str, struct ipaddr *addr)
 
 int strToCidr(const char *str, struct ipaddr *this_cidr)
 {
-	int ok = 0;
+	int ok = FALSE;
 	char *tmp = strdup(str);
 	char *ptr = strchr(tmp, '/');
 	if(ptr != NULL)	*ptr = '\0';
 	
-	if(parseIpStr(tmp, this_cidr) && snmask(str, this_cidr)) ok = 1;
+	if(parseIpStr(tmp, this_cidr) && snmask(str, this_cidr)) ok = TRUE;
 	free(tmp);
 	return ok;
 }
@@ -170,7 +181,8 @@ int strToCidr(const char *str, struct ipaddr *this_cidr)
 
 void no_mem(int sz)
 {
-	errx(3, "Could not allocate %u bytes of memory.", sz);
+	warnx("Could not allocate %u bytes of memory.", sz);
+	abort();
 }
 
 void print_string_array(char **a)
@@ -184,17 +196,18 @@ void print_string_array(char **a)
 	warnx("--");
 }
 
+
 /* File functions */
 
-FILE * cidrfile_open()
+FILE * file_open(char *path)
 {
-	FILE *cidr_fhnd;
-	cidr_fhnd = fopen(cidr_file, "r");
-	if(cidr_fhnd == NULL) err(2, "open %s", cidr_file);
-	return cidr_fhnd;
+	FILE *fhnd;
+	fhnd = fopen(path, "r");
+	if(fhnd == NULL) err(EXIT_SYS_ERROR, "open %s", path);
+	return fhnd;
 }
 
-void cidrfile_close(FILE *hnd)
+void file_close(FILE *hnd)
 {
 	fclose(hnd);
 }
@@ -207,17 +220,54 @@ void slurp_eol(FILE *hnd)
 
 int cidrfile_next_netname(FILE *hnd, char *namebuf, int doSlurp)
 {
-	while(1)
+	while(TRUE)
 	{
 		if(feof(hnd)) break;
 		if(doSlurp && ftell(hnd) != 0) slurp_eol(hnd);
 		if(fscanf(hnd, "%s", namebuf) == 1)
 		{
 			if(strlen(namebuf) == 0 || namebuf[0] == '#') continue;
-			return 1;
+			return TRUE;
 		}
 	}
-	return 0;
+	return FALSE;
+}
+
+void append_to_ipaddr_list(struct ipaddr *new_ipaddr, struct ipaddr **list, unsigned int *n_element)
+{
+	int list_size_new = sizeof(struct ipaddr) * ((*n_element) + 1);
+	*list = (*n_element == 0) ? malloc(list_size_new) : realloc(*list, list_size_new);
+	if(*list == NULL) no_mem(list_size_new);
+	memcpy(&(*list)[*n_element], new_ipaddr, sizeof(struct ipaddr));
+	(*n_element)++;
+}
+
+int getCidrListByFile(const char *filepath, unsigned int *n_element, struct ipaddr **result_list)
+{
+	FILE *fhnd;
+	char cidrbuf[CIDR_MAX_LEN+1];
+	struct ipaddr this_cidr;
+	
+	/* Read CIDR list file */
+	fhnd = file_open(filepath);
+	
+	while(!feof(fhnd) && fscanf(fhnd, "%s", cidrbuf))
+	{
+		if(cidrbuf[0] != '#')
+		{
+			if(strToCidr(cidrbuf, &this_cidr))
+			{
+				/* cidrbuf is a valid subnet definition */
+				append_to_ipaddr_list(&this_cidr, result_list, n_element);
+			}
+			else
+			{
+				errx(EXIT_PARSE_ERROR, "Invalid CIDR definition '%s' in file '%s'", cidrbuf, filepath);
+			}
+		}
+	}
+	file_close(fhnd);
+	return TRUE;
 }
 
 int getCidrListByName(const char *netname, unsigned int *n_element, struct ipaddr **result_list, struct linked_string *alias_list)
@@ -228,14 +278,14 @@ int getCidrListByName(const char *netname, unsigned int *n_element, struct ipadd
 		result_list		array of found CIDRs
 		ptr_alias_stack	pointer to an array of strings each holding a subnet name to be resolved and terminated 
 						by a NULL element, or ptr_alias_stack == NULL if there is no array initialized
-		return			found: 1, not found: 0
+		return			found: TRUE, not found: FALSE
 	 */
 	FILE *cidr_fhnd;
 	char namebuf[NETNAME_MAX_LEN+1];
-	char cidrbuf[512];
-	char buf2[2];
+	char cidrbuf[NETNAME_MAX_LEN+1];
+	char buf2char[2];
 	unsigned int n_hit = 0;
-	int netname_found = 0;
+	int netname_found = FALSE;
 	unsigned int i, alias_list_no;
 
 	/* Maintain stack of aliases */
@@ -252,50 +302,52 @@ int getCidrListByName(const char *netname, unsigned int *n_element, struct ipadd
 		if(str == NULL) break;
 		if(strcmp(str, netname) == 0)
 		{
-			errx(2, "Subnet resolving loop detected at '%s'", netname);
+			errx(EXIT_PARSE_ERROR, "Subnet resolving loop detected at '%s'", netname);
 		}
 		i++;
 	}
 	alias_list_no = linked_string_add(alias_list, netname);
 	
 	
-	/* Read file */
-	cidr_fhnd = cidrfile_open();
+	/* Read Alias+CIDR definitions file */
+	cidr_fhnd = file_open(cidr_file);
 	
 	while(cidrfile_next_netname(cidr_fhnd, namebuf, NOSLURP))
 	{
 		if(strcmp(netname, namebuf)==0)
 		{
 			int tokens;
-			netname_found = 1;
+			netname_found = TRUE;
 
 			/* Try to resolve named subnet */
 			//warnx("NAME '%s'", namebuf);
 			next_token:
-				tokens = fscanf(cidr_fhnd, "%s%1[\n]", cidrbuf, buf2);
+				/* Read one word and optional newline char */
+				tokens = fscanf(cidr_fhnd, "%"NETNAME_MAX_LEN_STR"s%1[\n]", cidrbuf, buf2char);
 				if(tokens > 0)
 				{
 					//warnx("  CIDR '%s'", cidrbuf);
 					struct ipaddr this_cidr;
-					if(strToCidr(cidrbuf, &this_cidr))
+					if(cidrbuf[0] == '.' || cidrbuf[0] == '/')
+					{
+						/* cidrbuf is a path */
+						if(!getCidrListByFile(cidrbuf, n_element, result_list))
+						{
+							errx(EXIT_PARSE_ERROR, "File error at '%s'", cidrbuf);
+						}
+					}
+					else if(strToCidr(cidrbuf, &this_cidr))
 					{
 						/* cidrbuf is a valid subnet definition */
-						int sz = sizeof(struct ipaddr) * ((*n_element) + 1);
-						*result_list = (*n_element == 0) ? malloc(sz) : realloc(*result_list, sz);
-						if(*result_list == NULL)
-						{
-							no_mem(sz);
-						}
-						memcpy(&(*result_list)[*n_element], &this_cidr, sizeof(struct ipaddr));
+						append_to_ipaddr_list(&this_cidr, result_list, n_element);
 						n_hit++;
-						(*n_element)++;
 					}
 					else
 					{
 						/* cidrbuf is a network name or an invalid subnet definition */
 						if(!getCidrListByName(cidrbuf, n_element, result_list, alias_list))
 						{
-							errx(2, "Invalid CIDR definition '%s'", cidrbuf);
+							errx(EXIT_PARSE_ERROR, "Invalid CIDR definition '%s'", cidrbuf);
 						}
 					}
 					
@@ -311,7 +363,7 @@ int getCidrListByName(const char *netname, unsigned int *n_element, struct ipadd
 		}
 	}
 	
-	cidrfile_close(cidr_fhnd);
+	file_close(cidr_fhnd);
 	
 	/* pop stack */
 	linked_string_del(alias_list, alias_list_no);
@@ -325,8 +377,8 @@ int getCidrListByName(const char *netname, unsigned int *n_element, struct ipadd
 
 int in_subnet(struct ipaddr addr, struct ipaddr subnet)
 {
-	if(addr.af != subnet.af) return 0;
-	if(subnet.mask == 0) return 1;
+	if(addr.af != subnet.af) return FALSE;
+	if(subnet.mask == 0) return TRUE;
 	
 	int shift = ADDRLEN_BY_AF(addr.af) - subnet.mask;
 	if(addr.af == AF_INET)
@@ -341,7 +393,7 @@ int in_subnet(struct ipaddr addr, struct ipaddr subnet)
 	{
 		int off;
 		int rmask = subnet.mask;
-		int ok = 1;
+		int ok = TRUE;
 		for(off = 0; off < 16; off++)
 		{
 			uint8_t a, n, m;
@@ -354,7 +406,7 @@ int in_subnet(struct ipaddr addr, struct ipaddr subnet)
 			//fprintf(stderr, "Test [%02X] %02X in [%02X] %02X / [%d] %d\n", a, a & m, n, n & m, subnet.mask, m);
 			if((a & m) != (n & m))
 			{
-				ok = 0;
+				ok = FALSE;
 				break;
 			}
 			
@@ -365,7 +417,7 @@ int in_subnet(struct ipaddr addr, struct ipaddr subnet)
 	}
 	else
 	{
-		errx(2, "Unsupported Address Family 0x%X", addr.af);
+		errx(EXIT_SYS_ERROR, "Unsupported Address Family 0x%X", addr.af);
 	}
 }
 
@@ -378,16 +430,16 @@ int in_named_subnet(struct ipaddr addr, const char *netname)
 	{
 		while(c_net < n_elem)
 		{
-			if(in_subnet(addr, subnet_list[c_net])) return 0;
+			if(in_subnet(addr, subnet_list[c_net])) return IN_NAMED_SUBNET_YES;
 			c_net++;
 		}
 		free(subnet_list);
 	}
 	else
 	{
-		return 2;
+		return IN_NAMED_SUBNET_WRONG;
 	}
-	return 1;
+	return IN_NAMED_SUBNET_NO;
 }
 
 
@@ -409,18 +461,18 @@ int main(int argc, char **argv)
 	if(argc == 2)
 	{
 		char netname[NETNAME_MAX_LEN+1];
-		FILE *hnd = cidrfile_open();
+		FILE *hnd = file_open(cidr_file);
 		
 		while(cidrfile_next_netname(hnd, netname, YESSLURP))
 		{
 			switch(in_named_subnet(addr, netname))
 			{
-				case 0:
+				case IN_NAMED_SUBNET_YES:
 					printf("%s\n", netname);
 					break;
 			}
 		}
-		cidrfile_close(hnd);
+		file_close(hnd);
 		return EXIT_SUCCESS;
 	}
 	else if(argc > 2)
@@ -436,11 +488,11 @@ int main(int argc, char **argv)
 			{
 				switch(in_named_subnet(addr, argv[c_arg]))
 				{
-					case 0:
+					case IN_NAMED_SUBNET_YES:
 						return EXIT_SUCCESS;
 						break;
-					case 2:
-						errx(3, "Unknown network '%s'", argv[c_arg]);
+					case IN_NAMED_SUBNET_WRONG:
+						errx(EXIT_UNKNOWN_SUBNET, "Unknown network '%s'", argv[c_arg]);
 						break;
 				}
 			}
@@ -460,11 +512,13 @@ int main(int argc, char **argv)
 "Exit code:\n"
 "  %3d         one of the given subnets contains the address\n"
 "  %3d         none contains the address\n"
-"  %3d         unknown subnet name given\n"
-"    *         error\n"
-"Envirnment:\n"
+"  %3d         unknown subnet name\n"
+"  %3d         parse error\n"
+"  %3d         internal/system error\n"
+"    *         other error\n"
+"Environment:\n"
 "  CIDR_FILE   path for network aliases file\n",
-			CIDR_FILE, EXIT_SUCCESS, EXIT_FAILURE, 3);
+			CIDR_FILE, EXIT_SUCCESS, EXIT_FAILURE, EXIT_UNKNOWN_SUBNET, EXIT_PARSE_ERROR, EXIT_SYS_ERROR);
 	}
-	return 2;
+	return EXIT_SYS_ERROR;
 }
